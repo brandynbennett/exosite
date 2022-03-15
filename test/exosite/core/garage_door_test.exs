08f84a29fe4d1e6e6d2b799c2ef8504baeccc1d4
brandynbennett <brandynlbennett@gmail.com>
2022-03-14 21:06:00 -0600

    ignore duplicate access codes

--- test/exosite/core/garage_door_test.exs
+++ test/exosite/core/garage_door_test.exs
@@ -5,11 +5,22 @@ defmodule Exosite.Core.GarageDoorTest do
   alias Exosite.Core.GarageDoor
   alias Exosite.Core.User
 
-  test "Adds new access code" do
+  test "add_access_code/2 Adds new access code" do
     user = user_fixture()
 
     %GarageDoor{access_codes: [access_code]} =
       GarageDoor.new()
+      |> GarageDoor.add_access_code("abc", user)
+
+    assert %AccessCode{user_id: user.id, code: "abc"} == access_code
+  end
+
+  test "add_access_code/2 Ignores duplicates" do
+    user = user_fixture()
+
+    %GarageDoor{access_codes: [access_code]} =
+      GarageDoor.new()
+      |> GarageDoor.add_access_code("abc", user)
       |> GarageDoor.add_access_code("abc", user)
 
     assert %AccessCode{user_id: user.id, code: "abc"} == access_code

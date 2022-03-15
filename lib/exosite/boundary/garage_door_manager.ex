b752e35a22410ebc3ffef50d926c4152a7af338a
brandynbennett <brandynlbennett@gmail.com>
2022-03-14 21:42:19 -0600

    add ability to get state from server

--- lib/exosite/boundary/garage_door_manager.ex
+++ lib/exosite/boundary/garage_door_manager.ex
@@ -1,12 +1,34 @@
 defmodule Exosite.Boundary.GarageDoorManager do
   use GenServer
+  alias Exosite.Core.GarageDoor
+  alias Exosite.Boundary.GarageDoorManager.State
 
   def new(name \\ __MODULE__, events \\ []) do
-    GenServer.start_link(__MODULE__, events, name: name)
+    GenServer.start_link(__MODULE__, State.new(events: events), name: name)
+  end
+
+  def get_state(name \\ __MODULE__) do
+    GenServer.call(name, :get_state)
   end
 
   @impl true
-  def init(events \\ []) do
-    {:ok, events}
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call(:get_state, _from, state) do
+    {:reply, state, state}
+  end
+end
+
+defmodule Exosite.Boundary.GarageDoorManager.State do
+  alias Exosite.Core.GarageDoor
+
+  defstruct door: nil, events: []
+
+  def new(fields \\ []) do
+    fields = Keyword.put_new(fields, :door, GarageDoor.new())
+    struct!(__MODULE__, fields)
   end
 end
--- test/exosite/boundary/garage_door_manager_test.exs
+++ test/exosite/boundary/garage_door_manager_test.exs
@@ -3,9 +3,15 @@ defmodule Exosite.Boundary.GarageDoorManagerTest do
 
   alias Exosite.Boundary.GarageDoorManager
 
-  test "new/0 creates new manager" do
+  test "new/0 creates new manager with initial state" do
     GarageDoorManager.new()
 
+    state = GarageDoorManager.get_state()
     assert is_pid(Process.whereis(GarageDoorManager))
+    assert state.events == []
+    assert state.door.state == :closed
+  end
+
+  test "" do
   end
 end
